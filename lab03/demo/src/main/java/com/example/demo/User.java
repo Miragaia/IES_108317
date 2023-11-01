d327b047845849476dd6c706250210e6b21a995e
Miragaia <miguelmiragaia2003@gmail.com>
2023-11-01 21:18:45 +0000

    lab03

--- lab03/demo/src/main/java/com/example/demo/User.java
+++ lab03/demo/src/main/java/com/example/demo/User.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 import com.fasterxml.jackson.core.StreamReadCapability;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class User {
